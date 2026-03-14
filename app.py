5a52339258544e2cf2643150c840779028939efe
deuj91 <dajmec@gmail.com>
2026-03-14 21:54:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ cache = Cache(config={
 
 cache.init_app(app)
 
-API_KEY = os.environ.get("RIOT_API_KEY")
+API_KEY = os.environ.get("RGAPI-17629850-7fb2-4282-ad49-3607656edeb0")
 
 REGION = "europe"
 PLATFORM = "euw1"
